84af6bf22ec87d140ad8af83811b070636bbae13
mahnoor faisal <faisalmahnoor86@gmail.com>
2024-04-25 21:19:11 +0500

    initial

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,27 +1,35 @@
-import Head from 'next/head';
-//import styles from '../styles/Home.module.css';
-import "@rainbow-me/rainbowkit/styles.css";
-import { ethers } from "ethers";
-// import { CONTRACTS } from "@/config/config";
-// import FAMEABI from "@/abi/fameABI.json";
+import { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
+import FAMEABI from '../public/src/fameABI.json';
+import { useFame } from './useFame.js';
 
-export default function Home() {
-  const rpcUrl = "https://rpc.ankr.com/polygon/8cd6b1cea2d3af26c0e267955e74bc8c41cd053b47d9fc04f3dacbf2c0c8557a"; // Replace with your Infura project ID or another RPC URL
-        console.log("Using fallback JSON-RPC provider:", rpcUrl);
+const useWallet = () => {
+
+  const [depositAmount, setDepositAmount] = useState(null);
+  const [gameDuration, setGameDuration] = useState(null);
+  const [ethersProvider, setEthersProvider] = useState(null);
+  const [contract, setcontract] = useState();
+
+  useEffect(() => {
+        const rpcUrl = 'https://rpc.ankr.com/polygon/8cd6b1cea2d3af26c0e267955e74bc8c41cd053b47d9fc04f3dacbf2c0c8557a';
         const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
+
+        const contractpolygon = new ethers.Contract(
+                    "0xE2186402701E73C81Ab9A6778a5bB6a94B51A745",
+                    FAMEABI,
+                    provider
+        );
         console.log(provider);
-        // const contractpolygon = new ethers.Contract(
-        //   "0xE2186402701E73C81Ab9A6778a5bB6a94B51A745",
-        //   FAMEABI,
-        //   provider
-        // );
-        // setcontract(contractpolygon)
-        // setEthersProvider(provider);
-        // setSigner(null); // No signer in this case
-        // setAddress(null); // No address in this case
+        console.log(contractpolygon);
+        setcontract(contractpolygon)
+        setEthersProvider(provider);
+   }, []);
+
   return (
     <div>
-      
+      <h1>Smart Contract Functions</h1>
     </div>
   );
 }
+
+export default useWallet;
