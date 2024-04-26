912d15fea519ced847748f635bf69ca7f6e486b9
mahnoor faisal <faisalmahnoor86@gmail.com>
2024-04-26 18:17:23 +0500

    fixed

--- pages/index.js
+++ pages/index.js
@@ -1,52 +1,51 @@
-import { useEffect, useState } from 'react';
-import Head from 'next/head';
-import { ethers } from 'ethers';
-import FAMEABI from '../fameABI.json';
+import { useEffect, useState } from "react";
+import Head from "next/head";
+import { ethers } from "ethers";
+import FAMEABI from "../fameABI.json";
 
 const useWallet = () => {
-
   const [depositAmount, setDepositAmount] = useState("$1000");
   const [gameDuration, setGameDuration] = useState(null);
   const [ethersProvider, setEthersProvider] = useState(null);
-  const [contract, setContract] = useState();
 
   useEffect(() => {
     async function connectToContract() {
-      try {
-        //url
-        const rpcUrl = 'https://rpc.ankr.com/polygon/8cd6b1cea2d3af26c0e267955e74bc8c41cd053b47d9fc04f3dacbf2c0c8557a';
-        const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
+      // change url
+      const rpcUrl = "https://polygon-rpc.com";
+      //const rpcUrl = 'https://rpc.ankr.com/polygon/8cd6b1cea2d3af26c0e267955e74bc8c41cd053b47d9fc04f3dacbf2c0c8557a';
+      const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
 
-        const contractPolygon = new ethers.Contract(
-          "0xE2186402701E73C81Ab9A6778a5bB6a94B51A745",
-          FAMEABI,
-          provider
-        );
+      const contractPolygon = new ethers.Contract(
+        "0xE2186402701E73C81Ab9A6778a5bB6a94B51A745",
+        FAMEABI,
+        provider
+      );
 
-        console.log(provider);
-        console.log(contractPolygon);
+      console.log(provider);
+      console.log(contractPolygon);
 
-        setContract(contractPolygon);
-        setEthersProvider(provider);
+      setEthersProvider(provider);
 
-        const deposit = await contractPolygon.depositAmount();
-        const duration = await contractPolygon.gameDuration();
-        setDepositAmount(deposit.toString());
-        setGameDuration(duration.toString());
-      } catch (error) {
-        console.error('Error connecting to contract:', error);
-      }
+      const deposit = await contractPolygon.depositAmount();
+      setDepositAmount(deposit.toString());
+
+      console.group("Deposit Function"); 
+      console.log(deposit);
+      console.log(depositAmount);
+      console.groupEnd();
+
+      const duration = await contractPolygon.gameDuration();
+      setGameDuration(duration.toString());
+      console.group("GameDuration Function"); 
+      console.log(duration);
+      console.log(gameDuration);
+      console.groupEnd();       
     }
 
     connectToContract();
   }, []);
 
-  return(
-    <div>
-      <h1>{depositAmount}</h1>
-      <h1>{gameDuration}</h1>
-    </div>
-  )
+  return <div></div>;
 };
 
 export default useWallet;
